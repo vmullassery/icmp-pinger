3243c8b931b2207a919cb62e82befd553e90ac53
vmullassery <66975097+vmullassery@users.noreply.github.com>
2022-05-01 22:48:43 -0500

    final

--- py3_icmp-pinger.py
+++ py3_icmp-pinger.py
@@ -152,7 +152,7 @@ def ping(host, maxIter, timeout=1):
     
     print("")
     print('-----Ping statstics for {}:-----'.format(host))
-    print('Packets: Sent = {}, Received = {}, Lost = ({}% loss)'.format(cnt, rtt_cnt, 100.0 - rtt_cnt * 100.0 / cnt))
+    print('Packets: Sent = {}, Received = {}, Lost = {} ({}% loss)'.format(cnt, rtt_cnt, cnt - rtt_cnt, 100.0 - rtt_cnt * 100.0 / cnt))
     if rtt_cnt != 0:
         print('Average delay is {:.7f}ms, min delay is {:.7f}ms, max delay is {:.7f}ms'.format(rtt_sum / rtt_cnt, rtt_min, rtt_max))
     print("")
